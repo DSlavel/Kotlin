ceca35d92aef8d94f043a1b5ad0d7d696a2bf797
dslavel <diabloslavel@gmail.com>
2019-05-22 18:07:48 +0300

    19

--- stepic/tirl/app/src/main/java/com/dslavel/tirl/activities/Utils.kt
+++ stepic/tirl/app/src/main/java/com/dslavel/tirl/activities/Utils.kt
@@ -1,27 +1,21 @@
 package com.dslavel.tirl.activities
 
+import android.app.Activity
 import android.content.Context
 import android.text.Editable
 import android.text.TextWatcher
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageView
 import android.widget.Toast
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
+import com.dslavel.tirl.R
+import com.dslavel.tirl.utils.GlideApp
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.ValueEventListener
-
-class ValueEventListenerAdapter(val handler: (DataSnapshot) -> Unit) : ValueEventListener {
-    private val TAG = "ValueEventListenerAdapt"
-
-    override fun onDataChange(data: DataSnapshot) {
-        handler(data)
-    }
-
-    override fun onCancelled(error: DatabaseError) {
-        Log.e(TAG, "onCancelled: ", error.toException())
-    }
-}
 
 fun Context.showToast(text: String, duration: Int = Toast.LENGTH_SHORT) {
     Toast.makeText(this, text, duration).show()
@@ -40,4 +34,15 @@ fun coordinateBtnAndInputs(btn: Button, vararg inputs: EditText) {
     }
     inputs.forEach { it.addTextChangedListener(watcher) }
     btn.isEnabled = inputs.all { it.text.isNotEmpty() }
+}
+
+fun ImageView.loadUserPhoto(photoUrl: String?) {
+    if (!(context as Activity).isDestroyed) {
+        GlideApp.with(this).load(photoUrl).fallback(R.drawable.person).into(this)
+    }
+}
+
+fun Editable.toStringOrNull(): String? {
+    val str = toString()
+    return if (str.isEmpty()) null else str
 }
---
+++ stepic/tirl/app/src/main/java/com/dslavel/tirl/utils/FirebaseHelper.kt
@@ -0,0 +1,78 @@
+package com.dslavel.tirl.utils
+
+
+import android.app.Activity
+import android.net.Uri
+import com.dslavel.tirl.activities.loadUserPhoto
+import com.dslavel.tirl.activities.showToast
+import com.dslavel.tirl.models.User
+import com.google.firebase.auth.AuthCredential
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+import com.google.firebase.storage.UploadTask
+import kotlinx.android.synthetic.main.activity_edit_profile.*
+
+class FirebaseHelper(private val activity: Activity) {
+    private val mAuth: FirebaseAuth = FirebaseAuth.getInstance()
+    private val mDatabase: DatabaseReference = FirebaseDatabase.getInstance().reference
+    private val mStorage: StorageReference = FirebaseStorage.getInstance().reference
+
+    fun uploadUserPhoto(photo: Uri, onSuccess: (UploadTask.TaskSnapshot) -> Unit) {
+        mStorage.child("users/${mAuth.currentUser!!.uid}/photo").putFile(photo)
+            .addOnCompleteListener {
+                if (it.isSuccessful) {
+                    onSuccess(it.result)
+                } else {
+                    activity.showToast(it.exception!!.message!!)
+                }
+            }
+    }
+
+    fun updateUserPhoto(photoUrl: String, onSuccess: () -> Unit) {
+        mDatabase.child("users/${mAuth.currentUser!!.uid}/photo").setValue(photoUrl)
+            .addOnCompleteListener {
+                if (it.isSuccessful) {
+                    onSuccess()
+                } else {
+                    activity.showToast(it.exception!!.message!!)
+                }
+            }
+    }
+
+    fun updateUser(updates: Map<String, Any?>, onSuccess: () -> Unit) {
+        mDatabase.child("users").child(mAuth.currentUser!!.uid).updateChildren(updates)
+            .addOnCompleteListener {
+                if (it.isSuccessful) {
+                    onSuccess()
+                } else {
+                    activity.showToast(it.exception!!.message!!)
+                }
+            }
+    }
+
+    fun updateEmail(email: String, onSuccess: () -> Unit) {
+        mAuth.currentUser!!.updateEmail(email).addOnCompleteListener {
+            if (it.isSuccessful) {
+                onSuccess()
+            } else {
+                activity.showToast(it.exception!!.message!!)
+            }
+        }
+    }
+
+    fun reauthenticate(credential: AuthCredential, onSuccess: () -> Unit) {
+        mAuth.currentUser!!.reauthenticate(credential).addOnCompleteListener {
+            if (it.isSuccessful) {
+                onSuccess()
+            } else {
+                activity.showToast(it.exception!!.message!!)
+            }
+        }
+    }
+
+    fun currentUserReference(): DatabaseReference =
+        mDatabase.child("users").child(mAuth.currentUser!!.uid)
+}
